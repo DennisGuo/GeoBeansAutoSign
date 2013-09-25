4a1a4d1eaa3a115955dafad916c737080cd186f3
GUOHENGXI <GUOHENGXI@GUO-HENGXI>
2013-09-25 11:10:15 +0800

    完善日志显示

---
+++ src/guo/tool/AutoSignService.java
@@ -0,0 +1,106 @@
+package guo.tool;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Properties;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class AutoSignService extends TimerTask {
+
+	@Override
+	public void run() {
+		// 获取配置信息
+		Properties pro = getProperty();
+		this.doSign(pro);
+	}
+
+	/**
+	 * 执行自动登录
+	 * @param pro
+	 */
+	private void doSign(Properties pro) {
+		String urlStr = pro.getProperty("sign.url");
+		String user = pro.getProperty("sign.username");
+		String pwd = pro.getProperty("sign.password");
+		String postKV = "username="+user+"&password="+pwd;
+		try {
+			URL url = new URL(urlStr);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setDoOutput(true);
+			conn.setRequestMethod("POST");
+			OutputStream out = conn.getOutputStream();
+			out.write(postKV.getBytes());
+			out.flush();
+			out.close();
+			int status = conn.getResponseCode();
+			System.out.println("---AutoSign Executed // Username:"+user+" Password:"+pwd+" Response:"+status+"  ["+CalendarHelp.getDataTimeStr()+"]");
+			System.out.println(":)");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 获取配置信息
+	 * @return
+	 */
+	public static Properties getProperty() {
+		try {
+			File file = new File("resources/application.properties");
+			FileInputStream in = new FileInputStream(file);
+			Properties pro = new Properties();
+			pro.load(in);
+			return pro;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			System.out.println("-------------------------------------------");
+			System.out.println("---GeoBeansAutoSign Fired");
+			System.out.println("---Written by guohengxi.dennis@gmail.com");
+			System.out.println("---Vsersion beta 0.1");
+			System.out.println("-------------------------------------------");
+			Properties pro = getProperty();
+			String user = pro.getProperty("sign.username");
+			System.out.println("---Username to sign :"+user+"  ["+CalendarHelp.getDataTimeStr()+"]");
+			
+			Timer timer = new Timer();
+			
+			Double min = 20 + Math.random()*10;//随机分钟数
+			Double sec = Math.random()*10*6;//随机秒
+			
+			String timeStr = CalendarHelp.getNewTimeStr()+" 08:"+min.intValue()+":"+sec.intValue();
+			Date firstTime = CalendarHelp.getDate(timeStr);
+			
+			//如果启动时，时间已经超过指定时间，则往后延一天
+			if (new Date().after(firstTime)) {
+				Calendar cal = new GregorianCalendar();
+				cal.setTime(firstTime);
+				cal.add(Calendar.DATE, 1);
+				firstTime = cal.getTime();
+				System.out.println("---Delay, the next time to sign at ："+CalendarHelp.getMonitorTimeStr(firstTime)+"  ["+CalendarHelp.getDataTimeStr()+"]");
+			}
+			
+			long period = 1000*60*60*24;//24小时
+			timer.schedule(new AutoSignService(), firstTime, period);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
